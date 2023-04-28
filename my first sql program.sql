6fc4414aa426ce99ddf2befe247316646ca3654d
Praveen kumar <130967293+Praveenkumar2024@users.noreply.github.com>
2023-04-28 10:22:38 +0530

    Add files via upload

---
+++ my first sql program.sql
@@ -0,0 +1,11 @@
+-- create table student(
+-- student_id  int primary key not null,
+-- student_name varchar(30) not null ;
+-- student_contact int not null,
+-- sturdent_dept varchar(25) not null
+-- );
+-- select * from student;
+-- INSERT into student values(50,"praveen",765432,"ece");
+INSERT into student(student_id,student_name, student_contact, sturdent_dept) 
+values(38,"hksjdf",6789,"ece");
+
